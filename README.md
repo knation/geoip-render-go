d7a2910a33789c9f7280269a03daca4e88d00bfe
Kirk Morales <kirk.morales@gmail.com>
2021-12-06 12:16:29 -0700

    Initial web service

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# ipgeo-render-go
+# geoip-render-go
 Golang project for looking up geo from an IP. Meant to be deployed on Render.
+
+## Dependencies
+
+In order to use this project, you'll need a copy of your own [Maxmind GeoIP database](https://www.maxmind.com/en/geoip2-services-and-databases). You can sign up for the GeoLite2 database [here](https://www.maxmind.com/en/geolite2/signup?lang=en).
+
+This project then relies on the [oschwald/geoip2-golang](https://pkg.go.dev/github.com/oschwald/geoip2-golang) package for looking up an IP in a MaxMind GeoIP database. See the documentation for all of the queries that can be made and the resulting structs.
+
+## Environment variables
+
+This project relies on the following environment variables:
+
+| ENV Variable | Description                                                                | Required | Default   |
+|--------------|----------------------------------------------------------------------------|----------|-----------|
+| `GEO_PATH`   | The location of your Maxmind GeoIP database (e.g., `./GeoLite2-City.mmdb`) | Yes      | None      |
+| `MODE`       | The mode to launch the application in.                                     | No      | "release" |
+| `PORT`       | The port for the web service to listen on.                                 | No      | 3000      |
+
+## How to use
+
+### 1. Fork Repository
+
+Fork this repository into your own account.
+
+### 2. Include your Maxmind GeoIP database
+
+Include your Maxmind GeoIP database in the root of the project. For example, `./GeoLite2-City.mmdb`. Commit this to the repository.
+
+### 3. Update code
+
+Update the `handler()` function in `main.go` to make the appropriate query and return the response.
+
+The example in `main.go` uses the IP address provided to make a `City()` query and returns the zip code as JSON.
+
+### 4. Deploy to render
+
+Create a new web service on Render. Use your new repository and set the above environment variables to make them available at run time.
+
+### 5. Make request
+
+The web service listens for requests to `/:ipaddress`.
+
+## Notes
+
+- This project DOES NOT log web requests. It only prints status logs. If you want to log each request, you can add it to the Go Gin logger.
---
+++ main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+  "context"
+  "errors"
+  "log"
+  "net"
+	"net/http"
+  "os"
+  "os/signal"
+  "syscall"
+  "time"
+  "github.com/gin-gonic/gin"
+  "github.com/oschwald/geoip2-golang"
+)
+
+var serviceMode string = os.Getenv("MODE")
+var port string = os.Getenv("PORT")
+
+var geoDb *geoip2.Reader
+
+func main() {
+
+  if serviceMode == "" {
+    serviceMode = "release"
+  }
+
+  if port == "" {
+    port = "3000"
+  }
+
+  log.Printf("Starting `ipgeo` service in '%s' mode...\n", serviceMode)
+
+  // Set the run mode of gin (release/debug)
+  gin.SetMode(serviceMode)
+
+  router := gin.New()
+
+  // Recovery middleware recovers from any panics and writes a 500 if there was one.
+	router.Use(gin.Recovery())
+
+	router.GET("/:ip", handler)
+
+  srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: router,
+	}
+
+  // Open Maxmind database
+  var geoErr error
+  geoDb, geoErr = geoip2.Open(os.Getenv("GEO_FILE"))
+	if geoErr != nil {
+		log.Fatal(geoErr)
+	}
+	defer geoDb.Close()
+
+  // Start webserver in background to allow for graceful shutdown code below
+  go func() {
+    log.Printf("Listening on port %v...\n", port)
+    if err := srv.ListenAndServe(); err != nil && errors.Is(err, http.ErrServerClosed) {
+      log.Panicln(err.Error());
+		}
+  }()
+
+  // Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal)
+	// kill (no param) default send syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall.SIGKILL but can't be caught, so don't need to add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+  log.Println("Shutting down server...")
+
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+    log.Panicf("Server forced to shutdown: %s\n", err.Error())
+	}
+
+  log.Println("Server exiting")
+}
+
+func handler(c *gin.Context) {
+
+	ip := net.ParseIP(c.Param("ip"))
+  if ip == nil {
+    c.AbortWithStatus(400)
+    return
+  }
+
+  // Update below this line to get the data you want
+
+	record, err := geoDb.City(ip)
+	if err != nil {
+		log.Fatal(err)
+    c.AbortWithStatus(500)
+    return
+	}
+
+  c.JSON(200, gin.H{
+		"zip": record.Postal.Code,
+	})
+
+}
